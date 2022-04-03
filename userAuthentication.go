7d7959d6af857176bdd823fd80c1ad29233b4ba0
bkgoh27 <bengkoon.goh27@gmail.com>
2022-04-03 13:28:30 +0800

    welcome all the authenticated user with if else

--- userAuthentication.go
+++ userAuthentication.go
@@ -1,5 +1,25 @@
 package main
 
+import "fmt"
+
+func authenticateUser(name string) string {
+	greeting := "Welcome"
+
+	if name == "Admin" {
+		greeting = greeting + ", " + name + "!"
+	} else if name == "Robin" || name == "John" {
+		greeting += "!"
+	} else {
+		greeting = "Merry Men"
+	}
+	fmt.Println(greeting)
+	return greeting
+}
+
 func main() {
-	//Insert Code here
+	var name string
+	fmt.Println("Give me your name!")
+	fmt.Scanln(&name)
+
+	authenticateUser(name)
 }
